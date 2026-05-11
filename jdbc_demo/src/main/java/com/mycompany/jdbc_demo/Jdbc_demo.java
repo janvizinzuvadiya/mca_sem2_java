a76408330ba6d1df4e4b9f15b2514caf6f9e88d4
janvizinzuvadiya <zinzuvadiyajanvi4@gmail.com>
2026-05-12 01:00:50 +0530

    prepared statement started

--- jdbc_demo/src/main/java/com/mycompany/jdbc_demo/Jdbc_demo.java
+++ jdbc_demo/src/main/java/com/mycompany/jdbc_demo/Jdbc_demo.java
@@ -2,6 +2,7 @@ package com.mycompany.jdbc_demo;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
@@ -15,6 +16,21 @@ public class Jdbc_demo
                         {
                                    Connection cn = DriverManager.getConnection("jdbc:mysql://localhost:3306/test","root","");
                                    Statement st = cn.createStatement();
+                                   
+                                   PreparedStatement pst = cn.prepareStatement("insert into sales values (?,?,?)");
+                                   
+                                   for (int i=1; i<6;i++)
+                                   {
+                                       
+                                   }
+                                   
+                                   
+                                   
+                                   
+                                   
+                                   
+                                   
+                                   
 //                                   st.execute("create table sales (pd_no int, pd_name varchar(20),pd_price int)");
                               
 //                                   st.executeUpdate("insert into sales values (2,'Mouse',5000)");      
@@ -24,12 +40,12 @@ public class Jdbc_demo
 
 //                                     st.executeUpdate("update sales set pd_price=70000 where pd_no=1");
 
-                                      ResultSet rs=st.executeQuery("select * from sales");
-                                      
-                                      while(rs.next())
-                                      {
-                                          System.out.println(rs.getInt(1)+"  ||  "+rs.getString(2)+"  ||  "+rs.getInt(3));
-                                      }        
+//                                      ResultSet rs=st.executeQuery("select * from sales");
+//                                      
+//                                      while(rs.next())
+//                                      {
+//                                          System.out.println(rs.getInt(1)+"  ||  "+rs.getString(2)+"  ||  "+rs.getInt(3));
+//                                      }        
                         }
                         catch(Exception e)
                         {
