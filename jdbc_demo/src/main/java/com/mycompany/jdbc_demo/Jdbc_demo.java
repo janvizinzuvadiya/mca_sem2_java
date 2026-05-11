85f052a5caa07481762558e476d8484b64f52bc9
janvizinzuvadiya <zinzuvadiyajanvi4@gmail.com>
2026-05-12 00:17:53 +0530

    connection preparation Statement Interface

--- jdbc_demo/src/main/java/com/mycompany/jdbc_demo/Jdbc_demo.java
+++ jdbc_demo/src/main/java/com/mycompany/jdbc_demo/Jdbc_demo.java
@@ -1,29 +1,39 @@
 package com.mycompany.jdbc_demo;
-import java.sql.*;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 
-public class Jdbc_demo {
 
-    public static void main(String[] args) 
-    {
-        try
+public class Jdbc_demo 
+{
+        public static void main(String [] args)
         {
-            connection con= new connection();
-            
-            Connection cn= con.connect_db();
-            
-            Statement st = cn.createStatement();
-            
-//            creating Table
-//            st.execute("create table student (rollno int,name varchar(20))");
-            
-//            Inserting data
-               st.executeUpdate("insert into student values(1,'aaa'),(2,'bbb'),(3,'ccc')");
+                        try
+                        {
+                                   Connection cn = DriverManager.getConnection("jdbc:mysql://localhost:3306/test","root","");
+                                   Statement st = cn.createStatement();
+//                                   st.execute("create table sales (pd_no int, pd_name varchar(20),pd_price int)");
+                              
+//                                   st.executeUpdate("insert into sales values (2,'Mouse',5000)");      
+//                                   st.executeUpdate("insert into sales values (3,'Keyboard',000)");
+//                                   st.executeUpdate("insert into sales values (4,'Charger',4000)");
+//                                   st.executeUpdate("insert into sales values (5,'PC',35000)");
+
+//                                     st.executeUpdate("update sales set pd_price=70000 where pd_no=1");
+
+                                      ResultSet rs=st.executeQuery("select * from sales");
+                                      
+                                      while(rs.next())
+                                      {
+                                          System.out.println(rs.getInt(1)+"  ||  "+rs.getString(2)+"  ||  "+rs.getInt(3));
+                                      }        
+                        }
+                        catch(Exception e)
+                        {
+                                   System.err.println(e);
+                        }
         }
-        catch(SQLException e)
-        {
-            System.out.println("Falied..................................................!"+e);
-        }
-        
-    }
 }
